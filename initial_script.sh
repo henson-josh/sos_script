ec318e7e150fe43dba7d5c0bd552c4f7eac8208e
johenson@redhat.com <johenson@redhat.com>
2022-08-15 11:59:40 -0700

    added additional options, merged what I could with other (initial.sh) script

--- initial_script.sh
+++ initial_script.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+############################################################
+# Arrays/Variables/Options                                 #
+############################################################
+
 tar_files=($(ls *.tar.xz | awk -F. '{print $1}'))
 
-############################################################
-# Help                                                     #
-############################################################
+tar_file_array(){
+    if [ -e "*.tar.xz" ]
+    then
+        tar_files=($(/usr/bin/ls *.tar.xz | awk -F. '{print $1}'))
+    else
+        printf '%s\n\n'"\x1b[31mNo tar file(s) found in: $(pwd)\\x1b[0m "'%s\n\n'
+        exit 1
+    fi
+}
+
+enable_debug(){
+    debug=true
+}
+
 Help()
 {
    # Display Help
@@ -13,6 +28,7 @@ Help()
    echo "options:"
    echo "a     Print all information."
    echo "c     Remove SOS report directories"
+   echo "d     Enable debug."
    echo "h     Print this Help."
    echo "n     Print all nginx error.log warnings."
    echo "ps    Print all running ansible processes."
@@ -27,14 +43,24 @@ Help()
 # Get the options
 while [ -n "$1" ]; do # while loop starts
 
-	case "$1" in
-
-	-h) # Display help
+        case "$1" in
+        -c)
+            for file in "${tar_files[@]}"
+            do
+              printf '%s\n'" Removing directory: $file"'%s\n'
+              sudo rm --interactive=once -rf ${file}
+            done
+            exit;;
+        -d)
+            enable_debug
+            echo $debug
+            exit;;
+        -h) # Display help
             Help
-            exit;; 
+            exit;;
         -n) # Display nginx error.log warning messages.
             for file in "${tar_files[@]}"
-            do 
+            do
                 printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m nginx error.log:\n"
                 grep 'warn' $file/var/log/nginx/error.log
             done
@@ -45,17 +71,17 @@ while [ -n "$1" ]; do # while loop starts
                 printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m ansible processes running:\n"
                 grep ansible $file/ps
             done
-            exit;;		
+            exit;;
         -t) # Display Warning messages from tower.log (filtered scaling up/down messages)
             for file in "${tar_files[@]}"
-            do   
+            do
                 printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m tower.log warning messages:\n"
                 grep -v 'pid' $file/var/log/tower/tower.log | grep 'WARN'
             done
             exit;;
-	esac
+        esac
 
-	shift
+        shift
 
 done
 
@@ -63,21 +89,10 @@ done
 # Main program                                             #
 ############################################################
 
-# Verifying SOS Report tar file is present and extracting files.
-tar_file_array(){
-    if [ -e "*.tar.xz" ]
-    then
-	tar_files=($(/usr/bin/ls *.tar.xz | awk -F. '{print $1}'))
-    else
-	printf '%s\n\n'"\x1b[31mNo tar file(s) found in: $(pwd)\\x1b[0m "'%s\n\n'
-	exit 1
-    fi
-}
-
 if [ $Clean ]
 then
   for file in "${tar_files[@]}"
-  do 
+  do
       sudo rm -rf $file
   done
   exit 0
@@ -95,7 +110,6 @@ for file in "${tar_files[@]}"
     do
 
 # Removing /var/log/tower/ files older than 5 days
-printf '%s\n'"Removing /var/log/tower/ files older than 5 days."'%s\n'
 find $file/var/log/tower -mtime +5 -delete
 
 # Variables for high level overview of the system
@@ -113,3 +127,4 @@ printf " - ""\x1b[1;32m$ps\\x1b[0m ansible processes running
  - has \x1b[1;32mPython\\x1b[0m versions \n$(grep -i '^/usr/bin/python' $file/sos_commands/alternatives/alternatives_--display_python | awk -F/ '{printf "   - "$4"\n"}')
 "
 done 
+
