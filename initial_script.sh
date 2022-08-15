2542d19162f5a297febfa25434523116446882a4
johenson@redhat.com <johenson@redhat.com>
2022-08-15 11:26:36 -0700

    added additional options, merged what I could with other (initial.sh) script

--- initial_script.sh
+++ initial_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tar_files=($(ls *.tar.xz | awk -F. '{print $1}'))
+
 ############################################################
 # Help                                                     #
 ############################################################
@@ -7,11 +9,13 @@ Help()
 {
    # Display Help
    echo
-   echo "Syntax: whatever_the_final_name_will_be [-a|h|v|V]"
+   echo "Syntax: whatever_the_final_name_will_be [-a|c|h|n|ps|V]"
    echo "options:"
    echo "a     Print all information."
+   echo "c     Remove SOS report directories"
    echo "h     Print this Help."
    echo "n     Print all nginx error.log warnings."
+   echo "ps    Print all running ansible processes."
    echo "t     Print all tower.log warnings."
    echo "V     Print software version and exit."
    echo
@@ -21,28 +25,82 @@ Help()
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":h" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-   esac
+while [ -n "$1" ]; do # while loop starts
+
+	case "$1" in
+
+	-h) # Display help
+            Help
+            exit;; 
+        -n) # Display nginx error.log warning messages.
+            for file in "${tar_files[@]}"
+            do 
+                printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m nginx error.log:\n"
+                grep 'warn' $file/var/log/nginx/error.log
+            done
+            exit;;
+        -ps) # Display running ansible processes.
+            for file in "${tar_files[@]}"
+            do
+                printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m ansible processes running:\n"
+                grep ansible $file/ps
+            done
+            exit;;		
+        -t) # Display Warning messages from tower.log (filtered scaling up/down messages)
+            for file in "${tar_files[@]}"
+            do   
+                printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m tower.log warning messages:\n"
+                grep -v 'pid' $file/var/log/tower/tower.log | grep 'WARN'
+            done
+            exit;;
+	esac
+
+	shift
+
 done
+
+
+#while getopts ":h:n:t" option; do
+#   case $option in
+#      h) # display Help
+#         Help
+#         exit;;
+#      n) 
+#         for file in $tar_files
+#	 do 
+#             printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m nginx error.log:\n"
+#             grep 'warn' $file/var/log/nginx/error.log
+#	 done
+#	 exit;;
+#      t) 
+#         for file in $tar_files
+#         do   
+#             printf "\nHost ""\x1b[31m\'$(cat $file/hostname)'\\x1b[0m tower.log warning messages:\n"
+#             grep -v 'pid' $file/var/log/tower/tower.log | grep 'WARN'
+#         done
+#         exit;; 
+#     \?) # Invalid option
+#         echo "Error: Invalid option"
+#	 Help
+#         exit;;
+#   esac
+#done
 
 ############################################################
 # Main program                                             #
 ############################################################
 
-# Verifying SOS Report tar file is present
-printf '%s\n'"Verifying SOS tarball is present in current directory."'%s\n'
-if  [ ! -e *.tar.xz ] 
-   then
-	printf '%s\n'"\x1b[31mNo SOS Report found.\\x1b[0m"'%s\n'
-        exit 2
-fi
+# Verifying SOS Report tar file is present and extracting files.
+tar_file_array(){
+    if [ -e "*.tar.xz" ]
+    then
+	tar_files=($(/usr/bin/ls *.tar.xz | awk -F. '{print $1}'))
+    else
+	printf '%s\n\n'"\x1b[31mNo tar file(s) found in: $(pwd)\\x1b[0m "'%s\n\n'
+	exit 1
+    fi
+}
 
-# Extracting all SOS tar files
-tar_files=($(ls *.tar.xz | awk -F. '{print $1}'))
 if [ $Clean ]
 then
   for file in "${tar_files[@]}"
@@ -51,29 +109,32 @@ then
   done
   exit 0
 fi
-
 for file in "${tar_files[@]}"
 do 
   if [ ! -d $file ]
   then
     tar xf $file.tar.xz
   fi
+done
+
+for file in "${tar_files[@]}"
+    do
 
 # Removing /var/log/tower/ files older than 5 days
 printf '%s\n'"Removing /var/log/tower/ files older than 5 days."'%s\n'
 find $file/var/log/tower -mtime +5 -delete
 
-# Variables
+# Variables for high level overview of the system
 hostname=$(cat $file/hostname)
 ps=$(grep -c ansible $file/ps)
 nginxErrorWarn=$(grep -c 'warn' $file/var/log/nginx/error.log)
-towerlogWarn=$(grep -v 'scaling' $file/var/log/tower/tower.log | grep -c 'WARN')
+towerlogWarn=$(grep -v 'pid' $file/var/log/tower/tower.log | grep -c 'WARN')
 
 # Printing high level overview of the system
 printf "\nHost ""\x1b[31m\'$hostname'\\x1b[0m has...\n"
 printf " - ""\x1b[1;32m$ps\\x1b[0m ansible processes running
  - ""\x1b[1;32m$nginxErrorWarn\\x1b[0m warnings in the nginx error.log
- - ""\x1b[1;32m$towerlogWarn\\x1b[0m warnings in the current tower.log file (minus scaling worker warnings)
+ - ""\x1b[1;32m$towerlogWarn\\x1b[0m warnings in the current tower.log file (filtered scaling up/down warnings)
  - has \x1b[31mAnsible\\x1b[0m versions \n$(grep -i '^ansible' $file/installed-rpms | awk '{printf "   - "$1"\n"}')
  - has \x1b[1;32mPython\\x1b[0m versions \n$(grep -i '^/usr/bin/python' $file/sos_commands/alternatives/alternatives_--display_python | awk -F/ '{printf "   - "$4"\n"}')
 "
