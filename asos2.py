407e5c89014def8bee7b028036016c0da0fbd0e5
michael-himes <carr3327@gmail.com>
2022-09-15 21:28:31 -0600

    Add recursive functions with awful names

--- asos2.py
+++ asos2.py
@@ -1,12 +1,32 @@
+import json
 import os, sys
 import subprocess 
+
+def fun(args, data, i=0):
+     if len(args) > 1:
+         for key in data.keys():
+             if key == args[i]: 
+                 i = i + 1
+                 if isinstance(data[key], dict):
+                     return fun(args, data[key], i)
+                 else:
+                     return data[key]
+     else:
+         return data[args[0]]
+
+def run(parent_key, json):
+    for key, value in json.items():
+        if isinstance(value, dict):
+            run(key, value)
+            next
+        else:
+            #json[key] = subprocess.check_output(value)
+            json[key] = subprocess.run(value, capture_output=True, shell=True).stdout.decode('utf-8')
+    return {parent_key: json}
 
 #current_pwd = os.system("pwd")
 current_pwd = str(subprocess.check_output("pwd", shell=True))[2:-3]
 
-if len(sys.argv) > 1:
-    # if variable change current_pwd to desired_pwd 
-    desired_pwd = sys.argv[1]
 
 tar_directories = []
 tar_files = []
@@ -17,11 +37,27 @@ for element in os.listdir():
         else:
             tar_directories.append(element)
 
+config_file = open('/home/mhimes/workplace/sos_script/asos.config')
+args_by_commands = json.load(config_file)
+print(tar_directories)
+for directory in tar_directories:
+for name, command in args_by_commands.items():
+    #os.chdir('sosreport-cpvra79a1172-03308392-2022-09-13-sirrngj')
+    
+    if isinstance(command, dict):
+        args_by_commands.update(run(name, command))
+        next
+    else:
+        args_by_commands[name] = subprocess.run(command, capture_output=True, shell=True).stdout.decode('utf-8')
 
-sos_directory = current_pwd + '/' + tar_directories[0]  
-hostname_path = sos_directory + '/sos_commands/host/hostname' 
-f = open(hostname_path, 'r')
-hostname = f.read()
-print(hostname)
+
+if len(sys.argv) > 1:
+    # if variable change current_pwd to desired_pwd 
+    desired_pwd = sys.argv[1]
+    print(fun(sys.argv[1:], args_by_commands))
 
 
+#Marks = {'Physics':67, 'Maths':87}
+#Internal_marks = {'Practical':48}
+#
+#Marks.update(internal_marks)
